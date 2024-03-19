7ffca3793e3eecb9dac104ab8d84ac00c7650971
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2024-03-19 14:28:55 +0100

    Added TAsk Review

--- src/pages/TaskReview.jsx
+++ src/pages/TaskReview.jsx
@@ -1,5 +1,388 @@
 const TaskReview = () => {
-    return <h1>TaskReview</h1>
+    return <div className="">
+    <div className="flex items-center bg-white text-center mt-[20px] text-gray-400">
+        <div className="w-[470px] ">
+            <p>Task Title</p>
+        </div>
+        <div className="w-[350px]">
+            <p>Status</p>
+        </div>
+        <div className="w-[120px]">
+            <p>Deadline</p>
+        </div>
+        <div className="w-[170px]">
+            <p>Members</p>
+        </div>
+
+    </div>
+    <div className="hold">
+        
+  
+    
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/Vector.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-red-100 text-center text-red-500  w-[120px] mb-[25px]">
+            <button><p>Not Approved</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+
+        <div className="flex ">
+            <div className="holder">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group2.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group3.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group1.svg" alt="" />
+            </div>
+
+
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+
+
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/minus.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
+            <button><p>Ongoing</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+        <div className="flex">
+            <div className="mr-[70px]">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+
+        </div>
+
+
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/minus.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
+            <button><p>Ongoing</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+        <div className="flex ">
+            <div className="holder">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group2.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group3.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group1.svg" alt="" />
+            </div>
+
+
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/check.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
+            <button><p>Ongoing</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+        <div className="flex ">
+            <div className="holder">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group2.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group3.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group1.svg" alt="" />
+            </div>
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/check.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
+            <button><p>Ongoing</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+        <div className="flex">
+            <div className="mr-[70px]">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/Vector.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-red-100 text-center text-red-500  w-[120px] mb-[25px]">
+            <button><p>Not Approved</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+        <div className="flex mr-[15px] ">
+            <div className="holder">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group2.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group3.svg" alt="" />
+            </div>
+
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+
+
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/minus.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-amber-100 text-center text-amber-500  w-[120px] mb-[25px]">
+            <button><p>Overdue</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+        <div className="flex">
+            <div className="mr-[70px]">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/check.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
+            <button><p>Ongoing</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+        <div className="flex">
+            <div className="mr-[70px]">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/minus.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
+            <button><p>Ongoing</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+        <div className="flex">
+            <div className="mr-[70px]">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+
+
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/minus.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-amber-100 text-center text-amber-500  w-[120px] mb-[25px]">
+            <button><p>Overdue</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+
+        <div className="flex ">
+            <div className="holder">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group2.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group3.svg" alt="" />
+            </div>
+            <div className="ml-[-10px] ">
+                <img src="/src/assets/member/Group1.svg" alt="" />
+            </div>
+
+
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+
+
+    <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+        <div className="flex items-center gap-2.5 pb-[25px] ">
+            <div className="w-[20px] ">
+                <img src="/src/assets/taskimg/check.png" alt="" />
+            </div>
+            <div className="note">
+                <p>Create presentation slide for meeting with MD concerning the..</p>
+            </div>
+        </div>
+        <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
+            <button><p>Ongoing</p></button>
+        </div>
+        <div className="mb-[25px]">
+            <p>29th March 2024</p>
+        </div>
+        <div className="flex">
+            <div className="mr-[70px]">
+                <img src="/src/assets/member/Group4.svg" alt="" />
+            </div>
+
+        </div>
+        <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+            <button><p>Submit</p></button>
+        </div>
+
+    </div>
+    <div className="w-[100%] h-px bg-gray-300 "></div>
+</div>
+</div>
 }
 
 export default TaskReview;
