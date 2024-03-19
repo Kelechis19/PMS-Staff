9090b5fa35a1cb73071cc2a3d7afc0183abc2dff
Sadiqkamba <sadiqkamba123@gmail.com>
2024-03-19 13:55:49 +0100

    Added the members to ongoing task

--- src/pages/OngoingTask.jsx
+++ src/pages/OngoingTask.jsx
@@ -30,6 +30,24 @@ const OngoingTask = () => {
             <div className="mb-[25px]">
                 <p>29th March 2024</p>
             </div>
+
+            <div className="flex  ">
+                <div className="holder">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group2.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group3.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group1.svg" alt="" />
+                </div>
+
+
+
+            </div>
             <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
                 <button><p>Submit</p></button>
             </div>
@@ -53,6 +71,14 @@ const OngoingTask = () => {
             <div className="mb-[25px]">
                 <p>29th March 2024</p>
             </div>
+            <div className="flex">
+                <div className="mr-[70px]">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+
+            </div>
+
+
             <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
                 <button><p>Submit</p></button>
             </div>
@@ -75,6 +101,23 @@ const OngoingTask = () => {
             <div className="mb-[25px]">
                 <p>29th March 2024</p>
             </div>
+            <div className="flex  ">
+                <div className="holder">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group2.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group3.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group1.svg" alt="" />
+                </div>
+
+
+
+            </div>
             <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
                 <button><p>Submit</p></button>
             </div>
@@ -97,6 +140,21 @@ const OngoingTask = () => {
             <div className="mb-[25px]">
                 <p>29th March 2024</p>
             </div>
+            <div className="flex  ">
+                <div className="holder">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group2.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group3.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group1.svg" alt="" />
+                </div>
+
+            </div>
             <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
                 <button><p>Submit</p></button>
             </div>
@@ -118,6 +176,12 @@ const OngoingTask = () => {
             </div>
             <div className="mb-[25px]">
                 <p>29th March 2024</p>
+            </div>
+            <div className="flex">
+                <div className="mr-[70px]">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+
             </div>
             <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
                 <button><p>Submit</p></button>
@@ -141,70 +205,18 @@ const OngoingTask = () => {
             <div className="mb-[25px]">
                 <p>29th March 2024</p>
             </div>
-            <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
-                <button><p>Submit</p></button>
-            </div>
+            <div className="flex mr-[15px] ">
+                <div className="holder">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group2.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group3.svg" alt="" />
+                </div>
 
-        </div>
-        <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
 
-
-        <div className="flex justify-around mt-[px] bg-white pt-[10px]">
-            <div className="flex items-center gap-2.5 pb-[25px] ">
-                <div className="w-[20px] ">
-                    <img src="/src/assets/taskimg/minus.png" alt="" />
-                </div>
-                <div className="note">
-                    <p>Create presentation slide for meeting with MD concerning the..</p>
-                </div>
-            </div>
-            <div className="bg-amber-100 text-center text-amber-500  w-[120px] mb-[25px]">
-                <button><p>Overdue</p></button>
-            </div>
-            <div className="mb-[25px]">
-                <p>29th March 2024</p>
-            </div>
-            <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
-                <button><p>Submit</p></button>
-            </div>
-
-        </div>
-        <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
-        <div className="flex justify-around mt-[px] bg-white pt-[10px]">
-            <div className="flex items-center gap-2.5 pb-[25px] ">
-                <div className="w-[20px] ">
-                    <img src="/src/assets/taskimg/check.png" alt="" />
-                </div>
-                <div className="note">
-                    <p>Create presentation slide for meeting with MD concerning the..</p>
-                </div>
-            </div>
-            <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
-                <button><p>Ongoing</p></button>
-            </div>
-            <div className="mb-[25px]">
-                <p>29th March 2024</p>
-            </div>
-            <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
-                <button><p>Submit</p></button>
-            </div>
-
-        </div>
-        <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
-        <div className="flex justify-around mt-[px] bg-white pt-[10px]">
-            <div className="flex items-center gap-2.5 pb-[25px] ">
-                <div className="w-[20px] ">
-                    <img src="/src/assets/taskimg/minus.png" alt="" />
-                </div>
-                <div className="note">
-                    <p>Create presentation slide for meeting with MD concerning the..</p>
-                </div>
-            </div>
-            <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
-                <button><p>Ongoing</p></button>
-            </div>
-            <div className="mb-[25px]">
-                <p>29th March 2024</p>
             </div>
             <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
                 <button><p>Submit</p></button>
@@ -229,6 +241,107 @@ const OngoingTask = () => {
             <div className="mb-[25px]">
                 <p>29th March 2024</p>
             </div>
+            <div className="flex">
+                <div className="mr-[70px]">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+
+            </div>
+            <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+                <button><p>Submit</p></button>
+            </div>
+
+        </div>
+        <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+        <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+            <div className="flex items-center gap-2.5 pb-[25px] ">
+                <div className="w-[20px] ">
+                    <img src="/src/assets/taskimg/check.png" alt="" />
+                </div>
+                <div className="note">
+                    <p>Create presentation slide for meeting with MD concerning the..</p>
+                </div>
+            </div>
+            <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
+                <button><p>Ongoing</p></button>
+            </div>
+            <div className="mb-[25px]">
+                <p>29th March 2024</p>
+            </div>
+            <div className="flex">
+                <div className="mr-[70px]">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+
+            </div>
+            <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+                <button><p>Submit</p></button>
+            </div>
+
+        </div>
+        <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+        <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+            <div className="flex items-center gap-2.5 pb-[25px] ">
+                <div className="w-[20px] ">
+                    <img src="/src/assets/taskimg/minus.png" alt="" />
+                </div>
+                <div className="note">
+                    <p>Create presentation slide for meeting with MD concerning the..</p>
+                </div>
+            </div>
+            <div className="bg-green-100 text-center text-green-500  w-[120px] mb-[25px]">
+                <button><p>Ongoing</p></button>
+            </div>
+            <div className="mb-[25px]">
+                <p>29th March 2024</p>
+            </div>
+            <div className="flex">
+                <div className="mr-[70px]">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+
+            </div>
+            <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
+                <button><p>Submit</p></button>
+            </div>
+
+        </div>
+        <div className="w-[100%] h-px bg-gray-300 mt-[-10px]"></div>
+
+
+        <div className="flex justify-around mt-[px] bg-white pt-[10px]">
+            <div className="flex items-center gap-2.5 pb-[25px] ">
+                <div className="w-[20px] ">
+                    <img src="/src/assets/taskimg/minus.png" alt="" />
+                </div>
+                <div className="note">
+                    <p>Create presentation slide for meeting with MD concerning the..</p>
+                </div>
+            </div>
+            <div className="bg-amber-100 text-center text-amber-500  w-[120px] mb-[25px]">
+                <button><p>Overdue</p></button>
+            </div>
+            <div className="mb-[25px]">
+                <p>29th March 2024</p>
+            </div>
+
+            <div className="flex  ">
+                <div className="holder">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group2.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group3.svg" alt="" />
+                </div>
+                <div className="ml-[-10px] ">
+                    <img src="/src/assets/member/Group1.svg" alt="" />
+                </div>
+
+
+
+            </div>
             <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
                 <button><p>Submit</p></button>
             </div>
@@ -251,6 +364,12 @@ const OngoingTask = () => {
             </div>
             <div className="mb-[25px]">
                 <p>29th March 2024</p>
+            </div>
+            <div className="flex">
+                <div className="mr-[70px]">
+                    <img src="/src/assets/member/Group4.svg" alt="" />
+                </div>
+
             </div>
             <div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white">
                 <button><p>Submit</p></button>
