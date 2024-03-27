638a232c46db825bb6197a15759f434743fa5da2
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2024-03-27 09:22:17 +0100

    Worked on the Complaints tab in the reports tab

--- src/pages/Goals.jsx
+++ src/pages/Goals.jsx
@@ -12,10 +12,10 @@ const Goals = () => {
   };
 
   return (
-    <div className=" w-full flex flex-col gap-[10px] ">
-      <div className="w-full">
+    <div className=" w-full flex flex-col gap-[10px] mt-2">
+      {/* <div className="w-full">
         <p className="font-bold text-[16px]">Organiztional Goals</p>
-      </div>
+      </div> */}
       <div className="w-full h-[45px]  bg-[white] rounded-[5px] flex  text-center">
         <button
           className={`text-[#205BB1] text-[16px] font-[700] flex-1 rounded-[5px] ${
@@ -27,7 +27,7 @@ const Goals = () => {
             handleButtonClick(1);
           }}
         >
-          Short-Term
+          Complaints
         </button>
         <button
           className={`text-[#205BB1] text-[16px] font-[700] flex-1  rounded-[5px]  ${
@@ -39,9 +39,9 @@ const Goals = () => {
             handleButtonClick(2);
           }}
         >
-          Medium-Term
+          Evaluation
         </button>
-        <button
+        {/* <button
           className={`text-[#205BB1] text-[16px] font-[700] flex-1  rounded-[5px]  ${
             activeView === 3 ? "bg-[#1D53A117]/10 shadow-[black]/10" : ""
           }`}
@@ -50,9 +50,9 @@ const Goals = () => {
           }}
         >
           Long-Term
-        </button>
+        </button> */}
       </div>
-      <div className=" w-full  bg-white rounded-[8px]  min-h-[70vh]">
+      {/* <div className=" w-full  bg-white rounded-[8px]  min-h-[70vh]">
         {goals.map((goalitem, key) => {
           return (
             <GoalEntry
@@ -71,15 +71,88 @@ const Goals = () => {
             <span>Create new Goal</span>
           </div>
         </div>
+      </div> */}
+      <div>
+        <div className="w-[full] h-[46px] bg-white rounded-tl-lg rounded-tr-lg shadow flex items-center">
+          <div className="text-zinc-500 text-[18px] font-bold  pl-[120px]  ">Title</div>
+          <div className="text-zinc-500 text-[18px] font-bold pl-[575px] ">To</div>
+          <div className="text-zinc-500 text-[18px] font-bold pl-[132px] ">Status</div>
+        </div>
+        
+        <div className="w-[full] h-[60px] bg-white shadow flex border-b-[3px] border-b-[#656667]/10 items-center">
+          <div className=" w-[6%] pl-[24px]">
+            <img src="../src/assets/taskimg/Check.svg" alt="" />
+          </div>
+          <div className="w-[54%]  text-[20px] text-zinc-500  text-start">Create presentation slides for ...</div>
+          <div className="w-[10%]  text-[20px] text-zinc-500  text-center">GM</div>
+           <div className="w-[18%] relative">
+              <div className=" ml-[65px] w-[100px] h-[30px] rounded-[4px] bg-[#74B72E] text-[#74B72E] bg-opacity-20 relative text-center">
+                <div className="relative justify-start w-full h-full text-center">Reviewed</div> 
+              </div>
+            </div>
+           <div className="ml-[28px]">
+            <button className="w-[71px] rounded border border-[#17417E] hover:bg-blue-300">View</button>
+           </div>
+        </div>
+        <div className="w-[full] h-[60px] bg-white shadow flex border-b-[3px] border-b-[#656667]/10 items-center">
+          <div className=" w-[6%] pl-[24px]">
+            <img src="../src/assets/taskimg/Check.svg" alt="" />
+          </div>
+          
+          <div className="w-[54%]  text-[20px] text-zinc-500  text-start">Create presentation slides for ...</div>
+          <div className="w-[10%]  text-[20px] text-zinc-500  text-center">HR</div>
+           <div className="w-[18%] relative">
+              <div className=" ml-[65px] w-[100px] h-[30px] rounded-[4px] bg-[#74B72E] text-[#74B72E] bg-opacity-20 relative text-center">
+                <div className="relative justify-start w-full h-full text-center">Reviewed</div> 
+              </div>
+            </div>
+           <div className="ml-[28px]">
+            <button className="w-[71px] rounded border border-[#17417E] hover:bg-blue-300">View</button>
+           </div>
+        </div>
+        
+        <div className="w-[full] h-[60px] bg-white shadow flex border-b-[3px] border-b-[#656667]/10 items-center">
+          <div className=" w-[6%] pl-[24px]">
+            <img src="../src/assets/taskimg/Check.svg" alt="" />
+          </div>
+          <div className="w-[54%]  text-[20px] text-zinc-500  text-start">Create presentation slides for ...</div>
+          <div className="w-[10%]  text-[20px] text-zinc-500  text-center">GM</div>
+           <div className="w-[18%] relative">
+              <div className=" ml-[65px] w-[100px] h-[30px] rounded-[4px] bg-[#F5BA45] text-[#F5BA45] bg-opacity-20 relative text-center">
+                <div className="relative justify-start w-full h-full text-center">Pending</div> 
+              </div>
+            </div>
+           <div className="ml-[28px]">
+            <button className="w-[71px] rounded border border-[#17417E] hover:bg-blue-300">View</button>
+           </div>
+        </div>
+        
+        <div className="w-[full] h-[60px] bg-white shadow flex border-b-[3px] border-b-[#656667]/10 items-center">
+          <div className=" w-[6%] pl-[24px]">
+            <img src="../src/assets/taskimg/Check.svg" alt="" />
+          </div>
+          <div className="w-[54%]  text-[20px] text-zinc-500  text-start">Create presentation slides for ...</div>
+          <div className="w-[10%]  text-[20px] text-zinc-500  text-center">HR</div>
+           <div className="w-[18%] relative">
+              <div className=" ml-[65px] w-[100px] h-[30px] rounded-[4px] bg-[#F5BA45] text-[#F5BA45] bg-opacity-20 relative text-center">
+                <div className="relative justify-start w-full h-full text-center">Pending</div> 
+              </div>
+            </div>
+           <div className="ml-[28px]">
+            <button className="w-[71px] rounded border border-[#17417E] hover:bg-blue-300">View</button>
+           </div>
+        </div>
+        
       </div>
     </div>
+    
   );
 };
 
 const GoalEntry = ({ title, achieved, date_created }) => {
   return (
     <div className="w-full h-[40px] flex justify-between px-[40px] hover:bg-[#f8f8ff]  transition-all duration-200 ease-in-out my-[20px]">
-      <div className=" flex gap-3 items-center">
+      <div className="flex items-center gap-3 ">
         <input type="checkbox" checked={achieved} />
         <label className="text-black text-[12px]">{title} Text enty </label>
       </div>
