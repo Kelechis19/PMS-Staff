06d1ec6f720bec4dfe60c805b151b8ec1d42e41e
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2024-03-29 23:01:12 +0100

    Added complaint button in report tab

--- src/pages/Goals.jsx
+++ src/pages/Goals.jsx
@@ -48,15 +48,21 @@ const Reports = () =>{
         
         <div className="">
 
-      {
-        Complaints.map((item, key) => {
-            return (
-                      <TaskRow title={item.title} status={item.status} to={item.to} />
-                    )
-           })
-      }
-            
-      </div>
+            {
+              Complaints.map((item, key) => {
+                  return (
+                            <TaskRow title={item.title} status={item.status} to={item.to} />
+                          )
+                })
+            }
+        </div>
+
+        <button className="relative items-center w-full h-12 bg-white pl-[970px] py-[5px] rounded-b-lg">
+          <div className="w-[146px] h-[37px] px-3 py-2 bg-blue-500 rounded shadow justify-center items-center gap-2 inline-flex">
+            <img className="w-[21px] h-[21px]" src="../src/assets/subtract.svg" />
+            <div className="text-xs font-bold text-center text-indigo-50 ">File a Complaint</div>
+          </div>
+        </button>
   </div>
   
     
@@ -111,7 +117,7 @@ const TaskRow = ({ title, status, to }) => {
           <div className="w-[54%]  text-[20px] text-zinc-500  text-start">{title}</div>
           <div className="w-[10%]  text-[20px] text-zinc-500  text-center">{to}</div>
            <div className="w-[18%] relative">
-              <div className=" ml-[65px] w-[100px] h-[30px] rounded-[4px] bg-[#74B72E] text-[#74B72E] bg-opacity-20 relative text-center">
+              <div className={` ml-[65px] w-[100px] h-[30px] rounded-[4px] ${status === "Received" ?"bg-[#74B72E] text-[#74B72E]":status ==="Pending" ?"bg-[#F5BA45] text-[#F5BA45]" :""}    bg-opacity-20   `}>
                 <div className="relative justify-start w-full h-full text-center">{status}</div> 
               </div>
             </div>
@@ -119,10 +125,6 @@ const TaskRow = ({ title, status, to }) => {
             <button className="w-[71px] rounded border border-[#17417E] hover:bg-blue-300">View</button>
            </div>
         </div>
-        
-        
-        
-        
         
       </div>
     </div>
