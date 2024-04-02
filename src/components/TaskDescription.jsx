76d2daa9ae206b6fedcca19bafe719e7ee04f0bf
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2024-04-02 16:16:23 +0100

    buttom div for the ongoing tasks

--- src/components/TaskDescription.jsx
+++ src/components/TaskDescription.jsx
@@ -6,10 +6,18 @@ const Desc = ({opendesc}) => {
         <>
             <div className={`absolute inset-0 w-screen h-screen  z-50 flex items-center justify-center overflow-auto bg-black/20`}>
                 <div className="absolute inset-0 z-10 flex items-center justify-center w-screen h-screen overflow-auto bg-black/20" onClick={()=>{opendesc()}}/>
-                <div className="absolute w-screen z-50 h-[65%] bottom-0 bg-slate-100 ">
-                    <div className="bg-white mg">
+                <div className="absolute w-screen z-50 h-[70%] bottom-0 bg-slate-100 rounded-lg">
+                    <div className="">
+                        <img className="ml-[1450px] mt-2  w-8 h-8" src="/src/assets/Close_round.svg" alt="lol" />
+                    </div> 
+                    <div className="flex hold"><div className="bg-white  h-[440px] ml-[20px] rounded-lg w-[1000px] ">
                         oerinhgnierugnirngoirng
                     </div>
+                    
+                    <div className="bg-white h-[430px] w-[430px] mt-[10px] ml-6 rounded-lg">kmgeuihgurgheuh
+                    </div>
+                    </div>
+                    
                 </div>
             </div>
         </>,
