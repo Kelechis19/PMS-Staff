9aa6c6fa7d10e5020901d14051ba8e1a2cdf1793
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2024-05-15 13:57:01 +0100

    Added the tailwind invisible scrollbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,13 +5,25 @@ import Nav from "./Nav";
 
 const Layout = ({ children }) => {
 	return (
-		<div className="bg-[#F1F4F9] h-[100vh] p-[40px] flex flex-row gap-[1.5rem] w-[100vw] relative">
-			<Nav />
-			<div className="w-full  relative">
-				<Header />
-				<Outlet />
+		<>
+			<div id="home"></div>
+			<div className=" bg-[#F1F4F9]   flex flex-row  relative w-full max-h-screen overflow-x-hidden py-[20px] pl-[20px] pr-[20px]">
+				<div className="relative w-[25%]">
+					<div className="fixed top-[30px]">
+						<Nav />
+					</div>
+				</div>
+
+				<div className="w-[78%]  h-full  relative flex flex-col items-center justify-center ">
+					<div className="w-[89%] fixed top-[10px] h-[80px] z-100  ">
+						<Header />
+					</div>
+					<div className="w-full h-full relative flex mt-[40px] ">
+						<Outlet />
+					</div>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
