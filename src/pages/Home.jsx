c15983b60ee17fefd977c38c22c9d3a2d1209301
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2024-05-15 16:49:35 +0100

    Working on the dashboard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,224 +28,188 @@ const Home = () => {
 	};
 
 	return (
-        <div className="flex flex-col bg-[#F1F4F9] w-full">
-            {complaintPopup ? <ComplaintPopup complain={complain} /> : ""}
-            {goalform && <GoalForm close={closeGoalForm} addGoal={setGoals} />}
+		<div className='flex flex-col bg-[#F1F4F9] w-full '>
+			{complaintPopup ? <ComplaintPopup complain={complain} /> : ""}
+			{goalform && <GoalForm close={closeGoalForm} addGoal={setGoals} />}
 
-            <div className="w-full gap-4 mt-[10px] flex">
-                <Link to="TaskHistory">
-                    <button>
-                        <div className="flex bg-white h-[160px] w-[280px] px-4 rounded-[8px] items-center p-[30px] gap-6 ">
-                            <div className="flex flex-col gap-4">
-                                <div>
-                                    <p className="text-[16px] font-[700]">
-                                        Overall Task Completed
-                                    </p>
-                                    <p className="font-[300] text-[12px] ">
-                                        28/56 tasks completed
-                                    </p>
-                                </div>
-                                <div className="relative w-[250px] bg-[#D9D9D9] h-[8px] rounded-full">
-                                    <div
-                                        className="absolute h-full bg-[#cdc307ed] rounded-full"
-                                        style={{
-                                            width: `${percentage1}%`,
-                                        }}></div>
-                                </div>
-                                <span className="text-[20px] font-[600] ">
-                                    {percentage1}%
-                                </span>
-                            </div>
-                        </div>
-                    </button>
-                </Link>
-                <Link to="TaskReview">
-                    <button>
-                        <div className="flex bg-white h-[160px] w-[300px] rounded-[8px] items-center p-[30px] gap-6 ">
-                            <div className="flex flex-col gap-4">
-                                <div>
-                                    <p className="text-[16px] font-[700]">
-                                        Average Task Review
-                                    </p>
-                                    <p className="font-[300] text-[12px] ">
-                                        Reviewed by GM on 13/05/2023
-                                    </p>
-                                </div>
+			<div className='w-full gap-4 mt-[10px] flex relative h-[160px]'>
+				<Link to='TaskHistory'>
+					<div className='flex bg-white h-full w-[300px] px-4 rounded-[8px] items-center gap-6 '>
+						<div className='flex flex-col gap-4 relative'>
+							<div>
+								<p className='text-[16px] font-[700]'>Overall Task Completed</p>
+								<p className='font-[300] text-[12px] '>28/56 tasks completed</p>
+							</div>
+							<div className='relative w-[250px] bg-[#D9D9D9] h-[8px] rounded-full'>
+								<div
+									className='absolute h-full bg-[#cdc307ed] rounded-full'
+									style={{
+										width: `${percentage1}%`,
+									}}></div>
+							</div>
+							<span className='text-[20px] font-[600] '>{percentage1}%</span>
+						</div>
+					</div>
+				</Link>
+				<Link to='TaskReview'>
+					<div className='flex bg-white h-full w-[300px] rounded-[8px] items-center p-[30px] gap-6 '>
+						<div className='flex flex-col gap-4'>
+							<div>
+								<p className='text-[16px] font-[700]'>Average Task Review</p>
+								<p className='font-[300] text-[12px] '>
+									Reviewed by GM on 13/05/2023
+								</p>
+							</div>
 
-                                <div className="relative w-[250px] bg-[#D9D9D9] h-[8px] rounded-full">
-                                    <div
-                                        className="absolute h-full rounded-full bg-lime-500"
-                                        style={{
-                                            width: `${percentage2}%`,
-                                        }}></div>
-                                </div>
+							<div className='relative w-[250px] bg-[#D9D9D9] h-[8px] rounded-full'>
+								<div
+									className='absolute h-full rounded-full bg-lime-500'
+									style={{
+										width: `${percentage2}%`,
+									}}></div>
+							</div>
 
-                                <div className="flex items-center justify-between">
-                                    <span className="text-[20px] font-[600] ">
-                                        {percentage2}%
-                                    </span>
-                                    <div className="text-lg font-normal text-black">
-                                        <p>Excellent</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </button>
-                </Link>
-                <button>
-                    {" "}
-                    <div className="w-[430px] h-[160px] bg-white rounded-[8px]">
-                        {<Date />}
-                    </div>
-                </button>
-            </div>
+							<div className='flex items-center justify-between'>
+								<span className='text-[20px] font-[600] '>{percentage2}%</span>
+								<div className='text-lg font-normal text-black'>
+									<p>Excellent</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</Link>
 
-            <div className="flex mt-[15px] justify-between px-2 flex-wrap">
-                <div className="">
-                    <div className=" bg-white   w-[515px]  items-center rounded-lg ">
-                        <div className="flex items-center justify-between p-2 pt-[-15px]">
-                            <div className="text-base font-bold text-black ">
-                                <p>Ongoing Tasks</p>
-                            </div>
-                            <Link to="OngoingTasks">
-                                <div className="flex items-center text-sm font-medium text-black ">
-                                    <button className="flex ">
-                                        See all{" "}
-                                        <img
-                                            src="/src/assets/star/arrow.svg"
-                                            alt=""
-                                        />
-                                    </button>
-                                </div>
-                            </Link>
-                        </div>
-                        <div>
-                            <TaskList tasks={tasks} />
-                        </div>
-                    </div>
-                </div>
-                <div className="hol2">
-                    <div className="w-[515px] bg-white rounded-lg p-3">
-                        <div className="flex items-center justify-between p-2 pt-[-15px]">
-                            <div className="text-base font-bold text-black ">
-                                <p>Complaints</p>
-                            </div>
-                            <Link to="OngoingTasks">
-                                <div className="flex items-center text-sm font-medium text-black ">
-                                    <button className="flex ">
-                                        See all{" "}
-                                        <img
-                                            src="/src/assets/star/arrow.svg"
-                                            alt=""
-                                        />
-                                    </button>
-                                </div>
-                            </Link>
-                        </div>
-                        <div className="text-black text-xs font-normal  pt-[6px] pb-[10px]">
-                            <p>
-                                You can send complaints or feedback to your
-                                General Manager
-                            </p>
-                        </div>
+				<div className='w-[500px] h-[160px] bg-white rounded-[8px]'>
+					{<Date />}
+				</div>
+			</div>
 
-                        <input
-                            className="w-[65%] h-[27px] border-[#00000033] border-1 text-wrap rounded outline-none"
-                            type="text"
-                        />
-                        <div className="pt-[7px] ">
-                            <p>Report</p>
-                        </div>
-                        <textarea
-                            className="w-[95%] h-[77px] border-[#00000033] border-1 mt-[8px] text-sm  p-2 text-wrap rounded outline-none "
-                            placeholder="Write Complaint"
-                            type="text"
-                        />
+			<div className='flex mt-[15px] justify-between px-2 flex-wrap'>
+				<div className=''>
+					<div className=' bg-white   w-[515px]  items-center rounded-lg '>
+						<div className='flex items-center justify-between p-2 pt-[-15px]'>
+							<div className='text-base font-bold text-black '>
+								<p>Ongoing Tasks</p>
+							</div>
+							<Link to='OngoingTasks'>
+								<div className='flex items-center text-sm font-medium text-black '>
+									<button className='flex '>
+										See all <img src='/src/assets/star/arrow.svg' alt='' />
+									</button>
+								</div>
+							</Link>
+						</div>
+						<div>
+							<TaskList tasks={tasks} />
+						</div>
+					</div>
+				</div>
+				<div className='hol2'>
+					<div className='w-[515px] bg-white rounded-lg p-3'>
+						<div className='flex items-center justify-between p-2 pt-[-15px]'>
+							<div className='text-base font-bold text-black '>
+								<p>Complaints</p>
+							</div>
+							<Link to='OngoingTasks'>
+								<div className='flex items-center text-sm font-medium text-black '>
+									<button className='flex '>
+										See all <img src='/src/assets/star/arrow.svg' alt='' />
+									</button>
+								</div>
+							</Link>
+						</div>
+						<div className='text-black text-xs font-normal  pt-[6px] pb-[10px]'>
+							<p>You can send complaints or feedback to your General Manager</p>
+						</div>
 
-                        <button
-                            className="w-[130px] h-[30px] bg-[#4D7CC1] rounded shadow ml-[68%] mt-[10px] mb-[8px] "
-                            onClick={complain}>
-                            <div className="text-xs font-semibold text-indigo-50 ">
-                                Submit
-                            </div>
-                        </button>
-                    </div>
-                </div>
-                <div className="hol3">
-                    <div className="w-[515px] bg-white rounded-lg  mt-[-150px]">
-                        <div className="flex items-center justify-between p-2 pt-[-15px]">
-                            <div className="text-base font-bold text-black ">
-                                <p>Task Review</p>
-                            </div>
-                            <div className="flex items-center gap-1 text-sm font-medium text-black">
-                                <Link to="OngoingTasks">
-                                    <div className="flex">
-                                        <button>See all</button>
-                                        <div className="relative w-6 h-6">
-                                            <img
-                                                src="/src/assets/star/arrow.svg"
-                                                alt=""
-                                            />
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className="ji">
-                            <ONGOING />
-                            <ONGOING />
-                        </div>
-                    </div>
-                </div>
-                <div className="hol4">
-                    <div className="w-[515px]  bg-white rounded-lg px-[15px] mt-[10px] p-2">
-                        <div className="text-base font-bold text-black ">
-                            <p>Peer Reviews</p>
-                        </div>
-                        <div className="flex items-center justify-between mt-[5px] pt-[10px] pb-[18px]">
-                            <div className="text-sm">
-                                <p>
-                                    review collage you have worked with
-                                    anoymously
-                                </p>
-                            </div>
-                            <div className="w-[142px] h-[38px] px-4 py-3 bg-[#4D7CC1] rounded border border-black border-opacity-0 justify-center items-center gap-2 inline-flex ">
-                                <div className="text-xs font-bold text-indigo-50 ">
-                                    Review Colleagues
-                                </div>
-                            </div>
-                        </div>
-                        <div className="">
-                            <PeerReviewRow />
-                        </div>
-                    </div>
-                </div>
-                <div className="hol5">
-                    <div className="w-[515px]  bg-white rounded-lg mt-[-64%]">
-                        <div className="flex items-center justify-between p-2 pt-[-15px]">
-                            <div className="text-base font-bold text-black ">
-                                <p>Comment</p>
-                            </div>
-                            <div className="flex items-center gap-1 text-sm font-medium text-black">
-                                <button>See all</button>
-                                <div className="relative w-6 h-6">
-                                    <img
-                                        src="/src/assets/star/arrow.svg"
-                                        alt=""
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="h-fit">
-                            <ChatMessage />
-                            <ChatMessage />
-                            <ChatMessage />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+						<input
+							className='w-[65%] h-[27px] border-[#00000033] border-1 text-wrap rounded outline-none'
+							type='text'
+						/>
+						<div className='pt-[7px] '>
+							<p>Report</p>
+						</div>
+						<textarea
+							className='w-[95%] h-[77px] border-[#00000033] border-1 mt-[8px] text-sm  p-2 text-wrap rounded outline-none '
+							placeholder='Write Complaint'
+							type='text'
+						/>
+
+						<button
+							className='w-[130px] h-[30px] bg-[#4D7CC1] rounded shadow ml-[68%] mt-[10px] mb-[8px] '
+							onClick={complain}>
+							<div className='text-xs font-semibold text-indigo-50 '>
+								Submit
+							</div>
+						</button>
+					</div>
+				</div>
+				<div className='hol3'>
+					<div className='w-[515px] bg-white rounded-lg  mt-[-150px]'>
+						<div className='flex items-center justify-between p-2 pt-[-15px]'>
+							<div className='text-base font-bold text-black '>
+								<p>Task Review</p>
+							</div>
+							<div className='flex items-center gap-1 text-sm font-medium text-black'>
+								<Link to='OngoingTasks'>
+									<div className='flex'>
+										<button>See all</button>
+										<div className='relative w-6 h-6'>
+											<img src='/src/assets/star/arrow.svg' alt='' />
+										</div>
+									</div>
+								</Link>
+							</div>
+						</div>
+						<div className='ji'>
+							<ONGOING />
+							<ONGOING />
+						</div>
+					</div>
+				</div>
+				<div className='hol4'>
+					<div className='w-[515px]  bg-white rounded-lg px-[15px] mt-[10px] p-2'>
+						<div className='text-base font-bold text-black '>
+							<p>Peer Reviews</p>
+						</div>
+						<div className='flex items-center justify-between mt-[5px] pt-[10px] pb-[18px]'>
+							<div className='text-sm'>
+								<p>review collage you have worked with anoymously</p>
+							</div>
+							<div className='w-[142px] h-[38px] px-4 py-3 bg-[#4D7CC1] rounded border border-black border-opacity-0 justify-center items-center gap-2 inline-flex '>
+								<div className='text-xs font-bold text-indigo-50 '>
+									Review Colleagues
+								</div>
+							</div>
+						</div>
+						<div className=''>
+							<PeerReviewRow />
+						</div>
+					</div>
+				</div>
+				<div className='hol5'>
+					<div className='w-[515px]  bg-white rounded-lg mt-[-64%]'>
+						<div className='flex items-center justify-between p-2 pt-[-15px]'>
+							<div className='text-base font-bold text-black '>
+								<p>Comment</p>
+							</div>
+							<div className='flex items-center gap-1 text-sm font-medium text-black'>
+								<button>See all</button>
+								<div className='relative w-6 h-6'>
+									<img src='/src/assets/star/arrow.svg' alt='' />
+								</div>
+							</div>
+						</div>
+						<div className='h-fit'>
+							<ChatMessage />
+							<ChatMessage />
+							<ChatMessage />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const PeerReviewRow = () => {
