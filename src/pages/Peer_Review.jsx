79e4fcc1690199d1bb461f8644f8e3bb076fb84e
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2024-05-20 14:58:15 +0100

    Update Peer_Review.jsx

--- src/pages/Peer_Review.jsx
+++ src/pages/Peer_Review.jsx
@@ -1,48 +1,43 @@
 import React, { useState } from "react";
 import { Departments } from "../data/temp";
-import { TopPerformerData } from "../data/temp";
 import { CiSearch } from "react-icons/ci";
-import { motion, AnimatePresence } from "framer-motion";
-import { useParams } from "react-router";
 
 const Peer_Review = () => {
 	const data = Departments;
 	const [emplpoyeeOpenPage, setEmployeeOpenPage] = useState(0);
 
 	return (
-		<>
-			<div className='bg-white h-full relative w-full p-2 '>
-				<div className=' relative w-full h-full items-center  '>
-					<div className='relative w-full h-[30px] flex bg-[white] items-center rounded-lg border border-[#00000040]'>
-						<span className='text-[20px] pl-[10px] pr-[5px] '>
-							<CiSearch />
-						</span>
+		<div className='bg-white h-full relative   mt-4 -m-2'>
+			{/* <div className=' relative  h-full items-center  '>
+				<div className='relative w-full h-[30px] flex bg-[white] items-center rounded-lg border border-[#00000040]'>
+					<span className='text-[20px] pl-[10px] pr-[5px] '>
+						<CiSearch />
+					</span>
 
-						<input
-							className='no-underline outline-none w-full rounded-[30px]'
-							placeholder='Search Employee'
-						/>
-					</div>
-					<div className='relative flex text-sm mt-6 font-semibold gap-3 items-center '>
-						<p>Department</p>
-						<div className='w-[50%] h-[40px] animate-appearance-in flex rounded-lg  '>
-							<div className=' items-center content-center w-full h-full rounded-lg bg-[#E9EFF7] '>
-								<select className='w-full outline-none rounded-lg bg-[#E9EFF7]'>
-									<option className='bg-white'>HOD</option>
-									<option className='bg-white'>GM</option>
-									<option className='bg-white'>HR</option>
-									<option className='bg-white'>All Departments</option>
-								</select>
-							</div>
+					<input
+						className='no-underline outline-none w-full rounded-[30px]'
+						placeholder='Search Employee'
+					/>
+				</div>
+				<div className='relative flex text-sm mt-6 font-semibold gap-3 items-center '>
+					<p>Department</p>
+					<div className='w-[50%] h-[40px] animate-appearance-in flex rounded-lg  '>
+						<div className=' items-center content-center w-full h-full rounded-lg bg-[#E9EFF7] '>
+							<select className='w-full outline-none rounded-lg bg-[#E9EFF7]'>
+								<option className='bg-white'>HOD</option>
+								<option className='bg-white'>GM</option>
+								<option className='bg-white'>HR</option>
+								<option className='bg-white'>All Departments</option>
+							</select>
 						</div>
 					</div>
-					<p className='pt-6 mb-6 '>Employeee</p>
-					<div className='pl-9 relative'>
-						<EmployeeRow />
-					</div>
 				</div>
-			</div>
-		</>
+				<p className='pt-6 mb-6 '>Employeee</p>
+				<div className='pl-9 relative'>
+					<EmployeeRow />
+				</div>
+			</div> */}ronorkmg
+		</div>
 	);
 };
 
@@ -57,7 +52,9 @@ const EmployeeRow = () => {
 				<div className='lol font-semibold'>{admindata.HOD.name}</div>
 				<div className=' font-normal'>{position[0]}</div>
 			</div>
-			<div className='font-normal ml-[100px] mr-24'>{admindata.HOD.position}</div>
+			<div className='font-normal ml-[100px] mr-24'>
+				{admindata.HOD.position}
+			</div>
 
 			{[...Array(5)].map((_, index) => (
 				<div
@@ -67,7 +64,9 @@ const EmployeeRow = () => {
 				</div>
 			))}
 
-			<div className="hello ml-10">iuorughoirengoi</div>
+			<div className='w-[130px] h-[30px] bg-black rounded shadow ml-[75%] mt-[10px] mb-[8px] '>
+				<div className='text-xs font-semibold text-indigo-50 '>Submit</div>
+			</div>
 		</div>
 	);
 };
