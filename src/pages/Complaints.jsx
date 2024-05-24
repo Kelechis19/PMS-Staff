6db4f38f26c6c29c902e7832cc08253d661a3974
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2024-05-24 21:37:33 +0100

    Small WOrks on the Complaints tab

--- src/pages/Complaints.jsx
+++ src/pages/Complaints.jsx
@@ -1,164 +1,152 @@
-import React from 'react'
+import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import { Departments } from "../data/temp";
+
 
 const Complaints = () => {
-  return (
-      <div>
-          <div className="mt-[10px] flex items-center border-1 border-White px-3 p-1 text-blue-300 rounded-md ">
-              <div className="">
-                  <p>Complaints</p>
-              </div>
-              <div className="ml-[950px]">
-                  <p>X</p>
-              </div>
-          </div>
-          <div className="flex bg-white p-2  mt-[10px] rounded-md">
-              <div className="pl-[5%]">
-                  <p>Title</p>
-              </div>
-              <div className="pl-[52%]">
-                  <p>To</p>
-              </div>
-              <div className="pl-[16%]">
-                  <p>Status</p>
-              </div>
-          </div>
-          <div className="hold">
-              <Comp />
-          </div>
-      </div>
-  );
-}
+	return (
+		<div className='w-full relative h-full -ml-4'>
+			<div className='mt-[10px] flex items-center border-1 justify-between border-White px-3 p-1 text-blue-300 rounded-md '>
+				<p>Complaints</p>
 
-const Comp = () => {
-  return (
-      <>
-          <div className="bg-white h-[500px] px-2 mt-[10px] rounded-lg">
-              <div className="flex items-center pt-[15px]">
-                  <div className="">
-                      <p>
-                          <span className="font-black ">.</span>{" "}
-                          <span className="pl-[8px]">
-                              Create presentation slides for..
-                          </span>
-                      </p>
-                  </div>
-                  <div className="pl-[38%]">
-                      <p>HR</p>
-                  </div>
-                  <div className="border-1 ml-[15%]  border-yellow-600 text-yellow-600 bg-yellow-200 w-[80px] px-2">
-                      <p>Pending</p>
-                  </div>
-                  <div className="border-1 border-black px-3  w-[60px] ml-[7%]">
-                      <p>View</p>
-                  </div>
-              </div>
-              <div className="border-1 border-gray-300 mt-[10px]"></div>
+				<CloseIcon />
+			</div>
+			<div className='flex bg-white p-2  mt-[10px] rounded-md'>
+				<div className='pl-[5%]'>
+					<p>Title</p>
+				</div>
+				<div className='pl-[52%]'>
+					<p>To</p>
+				</div>
+				<div className='pl-[16%]'>
+					<p>Status</p>
+				</div>
+			</div>
+			<div className='hold'>
+				<ComplaintsRow />{/*  I want this to just be the complaints like a single entity row  */}
+			</div>
+		</div>
+	);
+};
+/* See ehn you can look at the code at the tasks tab and see how to get the ongoing tasks tab and follow that syntax  */
+const ComplaintsRow = () => {
+	return (
+		<>
+			<div className='bg-white h-[500px] px-2 mt-[10px] rounded-lg'>
+				<div className='flex items-center pt-[15px]'>
+					<div className=''>
+						<p className="flex items-center ">
+							<span className='font-black '>.</span>{" "} {/* Do not use a fullstop instead import the svg from figma and align it centrally next to the text */}
+							<span className='pl-[8px]'>Create presentation slides for..</span>
+						</p>
+					</div>
+					<div className='pl-[38%]'>
+						<p>HR</p>
+					</div>
+					<div className='border-1 ml-[15%]  border-yellow-600 text-yellow-600 bg-yellow-200 w-[80px] px-2'>{/* Get the right colors from figma the hex value dont use talwind colors  */}
+						<p>Pending</p>
+					</div>
+					<div className='border-1 border-black px-3  w-[60px] ml-[7%]'>
+						<p>View</p>
+					</div>
+				</div>
+				<div className='border-1 border-gray-300 mt-[10px]'></div>
 
-              <div className="flex items-center pt-[15px]">
-                  <div className="">
-                      <p>
-                          <span className="font-black ">.</span>{" "}
-                          <span className="pl-[8px]">
-                              Create presentation slides for..
-                          </span>
-                      </p>
-                  </div>
-                  <div className="pl-[38%]">
-                      <p>HR</p>
-                  </div>
-                  <div className="border-1 ml-[15%]  border-yellow-600 text-yellow-600 bg-yellow-200 w-[80px] px-2">
-                      <p>Pending</p>
-                  </div>
-                  <div className="border-1 border-black px-3  w-[60px] ml-[7%]">
-                      <p>View</p>
-                  </div>
-              </div>
-              <div className="border-1 border-gray-300 mt-[10px]"></div>
+				<div className='flex items-center pt-[15px]'>
+					<div className=''>
+						<p>
+							<span className='font-black '>.</span>{" "}
+							<span className='pl-[8px]'>Create presentation slides for..</span>
+						</p>
+					</div>
+					<div className='pl-[38%]'>
+						<p>HR</p>
+					</div>
+					<div className='border-1 ml-[15%]  border-yellow-600 text-yellow-600 bg-yellow-200 w-[80px] px-2'>
+						<p>Pending</p>
+					</div>
+					<div className='border-1 border-black px-3  w-[60px] ml-[7%]'>
+						<p>View</p>
+					</div>
+				</div>
+				<div className='border-1 border-gray-300 mt-[10px]'></div>
 
-              <div className="flex items-center pt-[15px]">
-                  <div className="">
-                      <p>
-                          <span className="font-black ">.</span>{" "}
-                          <span className="pl-[8px]">
-                              Create presentation slides for..
-                          </span>
-                      </p>
-                  </div>
-                  <div className="pl-[38%]">
-                      <p>GM</p>
-                  </div>
-                  <div className="border-1 ml-[14.5%]  border-yellow-600 text-yellow-600 bg-yellow-200 w-[80px] px-2">
-                      <p>Pending</p>
-                  </div>
-                  <div className="border-1 border-black px-3  w-[60px] ml-[7.2%]">
-                      <p>View</p>
-                  </div>
-              </div>
-              <div className="border-1 border-gray-300 mt-[10px]"></div>
-              <div className="flex items-center pt-[15px]">
-                  <div className="">
-                      <p>
-                          <span className="font-black ">.</span>{" "}
-                          <span className="pl-[8px]">
-                              Create presentation slides for..
-                          </span>
-                      </p>
-                  </div>
-                  <div className="pl-[38%]">
-                      <p>HR</p>
-                  </div>
-                  <div className="border-1 ml-[15%]  border-green-600 text-green-500 bg-green-200 w-[80px] px-2">
-                      <p>Received</p>
-                  </div>
-                  <div className="border-1 border-black px-3  w-[60px] ml-[7%]">
-                      <p>View</p>
-                  </div>
-              </div>
-              <div className="border-1 border-gray-300 mt-[10px]"></div>
+				<div className='flex items-center pt-[15px]'>
+					<div className=''>
+						<p>
+							<span className='font-black '>.</span>{" "}
+							<span className='pl-[8px]'>Create presentation slides for..</span>
+						</p>
+					</div>
+					<div className='pl-[38%]'>
+						<p>GM</p>
+					</div>
+					<div className='border-1 ml-[14.5%]  border-yellow-600 text-yellow-600 bg-yellow-200 w-[80px] px-2'>
+						<p>Pending</p>
+					</div>
+					<div className='border-1 border-black px-3  w-[60px] ml-[7.2%]'>
+						<p>View</p>
+					</div>
+				</div>
+				<div className='border-1 border-gray-300 mt-[10px]'></div>
+				<div className='flex items-center pt-[15px]'>
+					<div className=''>
+						<p>
+							<span className='font-black '>.</span>{" "}
+							<span className='pl-[8px]'>Create presentation slides for..</span>
+						</p>
+					</div>
+					<div className='pl-[38%]'>
+						<p>HR</p>
+					</div>
+					<div className='border-1 ml-[15%]  border-green-600 text-green-500 bg-green-200 w-[80px] px-2'>
+						<p>Received</p>
+					</div>
+					<div className='border-1 border-black px-3  w-[60px] ml-[7%]'>
+						<p>View</p>
+					</div>
+				</div>
+				<div className='border-1 border-gray-300 mt-[10px]'></div>
 
-              <div className="flex items-center pt-[15px]">
-                  <div className="">
-                      <p>
-                          <span className="font-black ">.</span>{" "}
-                          <span className="pl-[8px]">
-                              Create presentation slides for..
-                          </span>
-                      </p>
-                  </div>
-                  <div className="pl-[38%]">
-                      <p>GM</p>
-                  </div>
-                  <div className="border-1 ml-[14.5%]  border-green-600 text-green-500 bg-green-200 w-[80px] px-2">
-                      <p>Received</p>
-                  </div>
-                  <div className="border-1 border-black px-3  w-[60px] ml-[7.2%]">
-                      <p>View</p>
-                  </div>
-              </div>
-              <div className="border-1 border-gray-300 mt-[10px]"></div>
-              <div className="flex items-center pt-[15px]">
-                  <div className="">
-                      <p>
-                          <span className="font-black ">.</span>{" "}
-                          <span className="pl-[8px]">
-                              Create presentation slides for..
-                          </span>
-                      </p>
-                  </div>
-                  <div className="pl-[38%]">
-                      <p>GM</p>
-                  </div>
-                  <div className="border-1 ml-[14.5%]  border-green-600 text-green-500 bg-green-200 w-[80px] px-2">
-                      <p>Received</p>
-                  </div>
-                  <div className="border-1 border-black px-3  w-[60px] ml-[7.2%]">
-                      <p>View</p>
-                  </div>
-              </div>
-              <div className="border-1 border-gray-300 mt-[10px]"></div>
-          </div>
-      </>
-  );
-}
+				<div className='flex items-center pt-[15px]'>
+					<div className=''>
+						<p>
+							<span className='font-black '>.</span>{" "}
+							<span className='pl-[8px]'>Create presentation slides for..</span>
+						</p>
+					</div>
+					<div className='pl-[38%]'>
+						<p>GM</p>
+					</div>
+					<div className='border-1 ml-[14.5%]  border-green-600 text-green-500 bg-green-200 w-[80px] px-2'>
+						<p>Received</p>
+					</div>
+					<div className='border-1 border-black px-3  w-[60px] ml-[7.2%]'>
+						<p>View</p>
+					</div>
+				</div>
+				<div className='border-1 border-gray-300 mt-[10px]'></div>
+				<div className='flex items-center pt-[15px]'>
+					<div className=''>
+						<p>
+							<span className='font-black '>.</span>{" "}
+							<span className='pl-[8px]'>Create presentation slides for..</span>
+						</p>
+					</div>
+					<div className='pl-[38%]'>
+						<p>GM</p>
+					</div>
+					<div className='border-1 ml-[14.5%]  border-green-600 text-green-500 bg-green-200 w-[80px] px-2'>
+						<p>Received</p>
+					</div>
+					<div className='border-1 border-black px-3  w-[60px] ml-[7.2%]'>
+						<p>View</p>
+					</div>
+				</div>
+				<div className='border-1 border-gray-300 mt-[10px]'></div>
+			</div>
+		</>
+	);
+};
 
-export default Complaints
+export default Complaints;
--- src/pages/OngoingTask.jsx
+++ src/pages/OngoingTask.jsx
@@ -1,30 +1,29 @@
 import React, { useState } from "react";
 import { Tasks } from "../data/temp";
 import People from "../components/Peopleimg";
-import { Link } from "react-router-dom";
-import Description from "./Description";
+
 import Modal from "../components/TaskSubmit";
 import Desc from "../components/TaskDescription";
 
 const OngoingTask = () => {
 	return (
-		<div className="">
-			<div className="flex items-center bg-white text-center mt-[20px] text-gray-400 rounded-lg">
-				<div className="w-[400px] ">
+		<div className=''>
+			<div className='flex items-center bg-white text-center mt-[20px] text-gray-400 rounded-lg'>
+				<div className='w-[400px] '>
 					<p>Task Title</p>
 				</div>
-				<div className="w-[205px]">
+				<div className='w-[205px]'>
 					<p>Status</p>
 				</div>
-				<div className="w-[250px]">
+				<div className='w-[250px]'>
 					<p>Deadline</p>
 				</div>
-				<div className="w-[170px]">
+				<div className='w-[170px]'>
 					<p>Members</p>
 				</div>
 			</div>
 
-			<div className=" pb-96">
+			<div className=' pb-96'>
 				{Tasks.map((item, key) => {
 					return (
 						<TaskRow
@@ -47,20 +46,18 @@ const TaskRow = ({ title, status, date, people }) => {
 	const modal = () => {
 		setIsOpen(!isOpen);
 	};
-	const [description, setDescription] = useState(true);
 
 	const opendesc = () => {
 		setDescOpen(!descOPen);
 	};
-	const [desc, setDesc] = useState(true);
 
 	return (
 		<>
-			<div className="relative w-full flex flex-row items-center px-[10px] justify-between  bg-white pt-[10px] border-b-[1px] border-b-[#656667]/10">
+			<div className='relative w-full flex flex-row items-center px-[10px] justify-between  bg-white pt-[10px] border-b-[1px] border-b-[#656667]/10'>
 				{isOpen && <Modal tile={title} modal={modal} />}
 				{descOPen && <Desc tile={title} opendesc={opendesc} />}
-				<div className="w-[35%] flex items-center gap-2.5 pb-[15px] ">
-					<div className="w-[30px]  ">
+				<div className='w-[35%] flex items-center gap-2.5 pb-[15px] '>
+					<div className='w-[30px]  '>
 						<img
 							src={`/src/assets/taskimg/${
 								status === "Not approved" ? "Warning.svg " : "minus.svg"
@@ -69,14 +66,14 @@ const TaskRow = ({ title, status, date, people }) => {
 					</div>
 
 					<button
-						className=" note text-start"
+						className=' note text-start'
 						onClick={() => {
 							opendesc();
 						}}>
 						<p>{title}</p>
 					</button>
 				</div>
-				<div className="relative w-[15%] flex ">
+				<div className='relative w-[15%] flex '>
 					<div
 						className={`flex  w-[100px] h-[30px] rounded-[4px] ${
 							status === "Not approved"
@@ -87,22 +84,22 @@ const TaskRow = ({ title, status, date, people }) => {
 								? "bg-amber-100 text-amber-300"
 								: ""
 						} text-center   w-[120px] mb-[25px]`}>
-						<p className="w-full h-full text-center">{status}</p>
+						<p className='w-full h-full text-center'>{status}</p>
 					</div>
 				</div>
 
-				<div className="w-[17%]">
+				<div className='w-[17%]'>
 					<p>{date}</p>
 				</div>
 
-				<div className="flex w-[10%]">
-					<div className="flex ">
+				<div className='flex w-[10%]'>
+					<div className='flex '>
 						<People people={people} />
 					</div>
 				</div>
 
-				<div className="bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white rounded">
-					<button onClick={modal} className="w-full">
+				<div className='bg-white text-center  w-[80px] mb-[23px] border-gray-500 border-[1px]  hover:bg-blue-500 hover:text-white rounded'>
+					<button onClick={modal} className='w-full'>
 						Submit
 					</button>
 				</div>
