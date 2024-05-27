6d1ba3b0fd510ae25937f33d718063db8637fdd6
Kelechis19 <161332923+Kelechis19@users.noreply.github.com>
2024-05-27 11:22:57 +0100

    Adding if statements for the  pages on

--- src/pages/SelfAppraisal.jsx
+++ src/pages/SelfAppraisal.jsx
@@ -8,23 +8,23 @@ import SelfAppraisalPage6 from "../components/SelfAppraisalPage6";
 import SelfAppraisalPage7 from "../components/SelfAppraisalPage7";
 import SelfAppraisalPage8 from "../components/SelfAppraisalPage8";
 
-
-
-
 const Self_Appraisal = () => {
-	const [currentPage, setCurrentPage] = useState(0);
-	const totalPages = 8;
-	const goToNextPage = () => {
-		if (currentPage < totalPages) {
-			setCurrentPage(currentPage + 1);
-		}
-	};
-	const goToPreviousPage = () => {
-		if (currentPage > 0) {
-			setCurrentPage(currentPage - 1);
-		}
-	};
-	const pages = [
+    const [currentPage, setCurrentPage] = useState(0);
+    const totalPages = 8;
+
+    const goToNextPage = () => {
+        if (currentPage < totalPages - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const goToPreviousPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const pages = [
         <SelfAppraisalPage1 key={0} />,
         <SelfAppraisalPage2 key={1} />,
         <SelfAppraisalPage3 key={2} />,
@@ -33,76 +33,89 @@ const Self_Appraisal = () => {
         <SelfAppraisalPage6 key={5} />,
         <SelfAppraisalPage7 key={6} />,
         <SelfAppraisalPage8 key={7} />,
+    
     ];
 
-	const percentage1 = (currentPage / totalPages) * 100;
-	return (
-		<>
-			<div className='w-full bg-white px-7 h-full p-2 rounded-lg relative mt-4'>
-				<div className='flex items-center gap-5 pt-[20px]'>
-					<div className='im'>
-						<img src='/src/assets/taskimg/Group330.svg' alt='' />
-					</div>
-					<div className='text'>
-						<div className='font-bold text-black'>
-							<p>Adoyiza omuya</p>
-						</div>
-						<div className='text-sm font-normal mt-[10px]'>
-							<p>General Manager</p>
-						</div>
-					</div>
-				</div>
+    const percentage1 = (currentPage / totalPages) * 100;
 
-				<div className='flex items-center justify-between mt-[20px]'>
-					<div className='flex flex-col gap-4 w-[250px]  '>
-						<div>
-							<p className='text-[16px] font-semibold'>
-								{(currentPage / totalPages) * 100} %Completed
-							</p>
-						</div>
-						<div className='relative w-full bg-[#D9D9D9] h-[8px] rounded-full'>
-							<div
-								className=' h-full bg-blue-400 rounded-full relative duration-1000'
-								style={{ width: `${percentage1}%` }}></div>
-						</div>
-					</div>
-					<div className='flex items-center gap-7 mt-[25px] text-white'>
-						<button
-							className='cursor pointer bg-[#4D7CC1] w-[120px]  p-1 cursor-pointer items-center justify-center flex border border-[#17417E] rounded'
-							onClick={goToPreviousPage}
-							disabled={currentPage === 0}>
-							Previous
-						</button>
-						<button
-							className='cursor-pointer bg-[#4D7CC1] w-[120px]  p-1 items-center justify-center flex border border-[#17417E] rounded'
-							onClick={goToNextPage}
-							disabled={currentPage === totalPages}>
-							Next
-						</button>
-					</div>
-				</div>
+    return (
+        <>
+            <div className='w-full bg-white px-7 h-full p-2 rounded-lg relative mt-4'>
+                <div className='flex items-center gap-5 pt-[20px]'>
+                    <div className='im'>
+                        <img src='/src/assets/taskimg/Group330.svg' alt='' />
+                    </div>
+                    <div className='text'>
+                        <div className='font-bold text-black'>
+                            <p>Adoyiza omuya</p>
+                        </div>
+                        <div className='text-sm font-normal mt-[10px]'>
+                            <p>General Manager</p>
+                        </div>
+                    </div>
+                </div>
 
-				<div className='font-bold mt-[26px]'>
-					<p>Instructions</p>
-				</div>
-				<div className='mt-[5px]'>
-					<p>
-						Evaluate yourself on the following listed factors by choosing a
-						score against each factor and entering details where necessary.
-					</p>
-				</div>
-				<div className='mt-[10px]'>
-					<div className='font-bold mb-[10px]'>
-						<p>Rating Key</p>
-					</div>
-					<div className=' mb-[12px]'>
-						<p>1 = Poor; 2 = fair; 3 = Good; 4 = Very Good; 5 = Outstanding; N/A - Not Applicable</p>
-					</div>
-				</div>
-				<div className="relative">{pages[currentPage]}</div>
-			</div>
-		</>
-	);
+                <div className='flex items-center justify-between mt-[20px]'>
+                    <div className='flex flex-col gap-4 w-[250px]'>
+                        <div>
+                            <p className='text-[16px] font-semibold'>{(currentPage / totalPages) * 100} %Completed</p>
+                        </div>
+                        <div className='relative w-full bg-[#D9D9D9] h-[8px] rounded-full'>
+                            <div
+                                className='h-full bg-blue-400 rounded-full relative duration-1000'
+                                style={{ width: `${percentage1}%` }}></div>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-7 mt-[25px] text-white'>
+                        <button
+                            className='cursor pointer bg-[#4D7CC1] w-[120px] p-1 cursor-pointer items-center justify-center flex border border-[#17417E] rounded'
+                            onClick={goToPreviousPage}
+                            disabled={currentPage === 0}>
+                            Previous
+                        </button>
+                        <button
+                            className='cursor-pointer bg-[#4D7CC1] w-[120px] p-1 items-center justify-center flex border border-[#17417E] rounded'
+                            onClick={goToNextPage}
+                            disabled={currentPage === totalPages - 1}>
+                            Next
+                        </button>
+                    </div>
+                </div>
+
+                {currentPage === totalPages  && (
+                    <>
+                        <div className='font-bold mt-[26px]'>
+                            <p>Instructions</p>
+                        </div>
+                        <div className='mt-[5px]'>
+                            <p>Evaluate yourself on the following listed factors by choosing a score against each factor and entering details where necessary.</p>
+                        </div>
+                        <div className='mt-[10px]'>
+                            <div className='font-bold mb-[10px]'>
+                                <p>Rating Key</p>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <p>1 = Poor; 2 = fair; 3 = Good; 4 = Very Good; 5 = Outstanding; N/A - Not Applicable</p>
+                            </div>
+                        </div>
+                    </>
+                )}
+
+                {currentPage === totalPages  && (
+                    <>
+                        <div className='font-bold mt-[26px]'>
+                            <p>Evaluation Recorded</p>
+                        </div>
+                        <div className='mt-[5px]'>
+                            <p>Your evaluation has been recorded.</p>
+                        </div>
+                    </>
+                )}
+
+                <div className="relative">{pages[currentPage]}</div>
+            </div>
+        </>
+    );
 };
 
 export default Self_Appraisal;
